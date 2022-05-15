2e0f445147e739d43602c82030650d97dc784b79
thefatrat1402 <gabinazpires@gmail.com>
2022-05-15 13:34:27 +0530

    Added youtube.py

--- youtube.py
+++ youtube.py
@@ -3,6 +3,18 @@
 import subprocess
 
 link = input("Youtube video link: ")
-print(link)
+print("1.360p")
+print("2.480p")
+print("3.720p")
+print("4.1080p")
+quality = input(":")
 
-p = subprocess.run(['youtube-dl', '-f', '137+140', link])
+if quality == "1":
+    subprocess.run(['youtube-dl', '-f', '134+140', link])
+elif quality == "2":
+    subprocess.run(['youtube-dl', '-f', '135+140', link])
+elif quality == "3":
+    subprocess.run(['youtube-dl', '-f', '136+140', link])
+elif quality == "4":
+    subprocess.run(['youtube-dl', '-f', '137+140', link])
+
